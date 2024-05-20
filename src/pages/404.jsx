37f3420869bc1cdb5fa664728f66474ab9253a9f
RUHAMYA Elie <163524403+ElieRu@users.noreply.github.com>
2024-05-20 17:29:15 +0200

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
     return <section className="d-flex justify-content-center align-items-center mt-5" style={{height: '550px'}}>
-            <div className="d-flex flex-column align-items-center"><img src="../assets/img/img-not-found.png" style={{width: '200px'}} />
+            <div className="d-flex flex-column align-items-center"><img src="https://raw.githubusercontent.com/ElieRu/manage-rest-api/main/src/assets/img/img-not-found.png" style={{width: '200px'}} />
                 <h4>Page not found</h4><span><a href="/">Go back to the homepage</a></span>
             </div>
         </section>

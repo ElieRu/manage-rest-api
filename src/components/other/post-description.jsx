b1084afc1b2b68f43467dc9a857bc87aae0286f7
RUHAMYA Elie <163524403+ElieRu@users.noreply.github.com>
2024-05-20 17:45:36 +0200

    Update post-description.jsx

--- src/components/other/post-description.jsx
+++ src/components/other/post-description.jsx
@@ -6,18 +6,16 @@ export default function PostDescription ({post}) {
         e.preventDefault()
     }
 
-    return <div className="card border-0">
-                <div className="card-body">
-                    <div>
-                        <h4 className="title-post text-capitalize">{post.title}</h4>
-                        <p className="text-capitalize">{post.body}</p>
-                    </div>
-                    <div>
-                        <form onSubmit={handleSubmit}>
-                            <textarea className="form-control mb-2" style={{resize: 'none'}} placeholder="Comment this post"></textarea>
-                            <button className="btn btn-primary" type="submit">Comment</button>
-                        </form>
-                    </div>
+    return <div>
+                <div>
+                    <h4 className="title-post text-capitalize">{post.title}</h4>
+                    <p className="text-capitalize">{post.body}</p>
+                </div>
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <textarea className="form-control mb-2" style={{resize: 'none'}} placeholder="Comment this post"></textarea>
+                        <button className="btn btn-primary" type="submit">Comment</button>
+                    </form>
                 </div>
             </div>
 }

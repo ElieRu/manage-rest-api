bb19ce0368e6a5f10ad831b40c2341f75628db36
RUHAMYA Elie <163524403+ElieRu@users.noreply.github.com>
2024-05-20 17:46:20 +0200

    Update post.jsx

--- src/pages/post.jsx
+++ src/pages/post.jsx
@@ -9,13 +9,11 @@ export default function Post() {
     let id = JSON.parse(useParams().id);
 
     return <section className="mt-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-6 col-lg-4 mb-3">
-                            <JsonPlaceholder data='posts' unique={true} id={id} />
-                        </div>
-                        <Comments id={id} />
+                <div className="row">
+                    <div className="col-12 col-md-6 col-lg-4 mb-3">
+                        <JsonPlaceholder data='posts' unique={true} id={id} />
                     </div>
+                    <Comments id={id} />
                 </div>
             </section>
 }
